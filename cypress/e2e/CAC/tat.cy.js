71e9db825cde3e5b17812de06378052b2a89f15c
Erickson De Souza Martinez <erickson.martinez@meta.com.br>
2024-12-23 23:29:49 -0300

    feat: typind fields after clean

--- cypress/e2e/CAC/tat.cy.js
+++ cypress/e2e/CAC/tat.cy.js
@@ -9,7 +9,7 @@ describe("Visit CAT TAT page", () => {
     cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
   });
 
-  it("Typing in filds and send form", () => {
+  it("Typing in fields and send form", () => {
     cy.get("#firstName").click().clear().type("Erickson");
     cy.get("#lastName").click().clear().type("Martinez");
     cy.get("#email").click().clear().type("teste@teste.com");
@@ -28,12 +28,40 @@ describe("Visit CAT TAT page", () => {
     cy.get("#open-text-area")
       .click()
       .clear()
-      .type("Typing in filds", { delay: 0 });
+      .type("Typing in field", { delay: 0 });
     cy.get('button[type="submit"]').should("have.text", "Enviar").click();
     cy.get(".error").should("be.visible");
   });
 
   it("message error phone invalid", () => {
     cy.get("#phone").type("abcdefgh").should("have.value", "");
+  });
+
+  it("Clean fields in form", () => {
+    cy.get("#firstName")
+      .click()
+      .type("Erickson")
+      .should("have.value", "Erickson")
+      .clear()
+      .should("have.value", "");
+    cy.get("#lastName")
+      .click()
+      .type("Martinez")
+      .should("have.value", "Martinez")
+      .clear()
+      .should("have.value", "");
+    cy.get("#email")
+      .click()
+      .type("teste@teste.com")
+      .should("have.value", "teste@teste.com")
+      .clear()
+      .should("have.value", "");
+    cy.get("#open-text-area")
+      .click()
+      .type("Typing in field", { delay: 0 })
+      .should("have.value", "Typing in field")
+      .clear()
+      .should("have.value", "");
+    cy.get('button[type="submit"]').should("have.text", "Enviar").click();
   });
 });
